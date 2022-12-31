20160d136fd7254bf95d871984e1acf31af909f7
Jacob Summerville <jsvikings97@gmail.com>
2022-12-31 07:49:57 -0800

    ChatApp Init

---
+++ 6-ChatApp/ChatApp/ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Jacob Summerville on 12/31/22.
+//
+
+import SwiftUI
+
+@main
+struct ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
