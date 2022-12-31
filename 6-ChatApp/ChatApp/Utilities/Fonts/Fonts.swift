b2b4475a75e943cdc7ce0c77415f2c74bae442b5
Jacob Summerville <jsvikings97@gmail.com>
2022-12-31 08:04:47 -0800

    ChatApp - setup

---
+++ 6-ChatApp/ChatApp/Utilities/Fonts/Fonts.swift
@@ -0,0 +1,47 @@
+//
+//  Fonts.swift
+//  ChatApp
+//
+//  Created by Jacob Summerville on 12/31/22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Font {
+    public static var bodyParagraph: Font {
+        return Font.custom("LexendDeca-Regular", size: 14)
+    }
+    
+    public static var button: Font {
+        return Font.custom("LexendDeca-SemiBold", size: 14)
+    }
+    
+    public static var smallText: Font {
+        return Font.custom("LexendDeca-Regular", size: 10)
+    }
+    
+    public static var tabBar: Font {
+        return Font.custom("LexendDeca-Regular", size: 12)
+    }
+    
+    public static var settings: Font {
+        return Font.custom("LexendDeca-Regular", size: 16)
+    }
+    
+    public static var titleText: Font {
+        return Font.custom("LexendDeca-Bold", size: 23)
+    }
+    
+    public static var pageTitle: Font {
+        return Font.custom("LexendDeca-SemiBold", size: 33)
+    }
+    
+    public static var chatHeading: Font {
+        return Font.custom("LexendDeca-SemiBold", size: 19)
+    }
+    
+    public static var chatName: Font {
+        return Font.custom("LexendDeca-SemiBold", size: 12)
+    }
+}
